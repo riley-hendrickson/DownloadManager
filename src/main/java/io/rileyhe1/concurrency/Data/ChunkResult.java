c3a1f40adb091af8a676a2128fba2daa2d237228
Riley Hendrickson <rileyhe1@gmail.com>
2025-11-18 16:06:05 -0800

    implemented ChunkResult class that serves as a report card for holding the result information for each chunk download operation. Implemented builder pattern to enforce immutability and thread safety as well as to improve readability

--- src/main/java/io/rileyhe1/concurrency/Data/ChunkResult.java
+++ src/main/java/io/rileyhe1/concurrency/Data/ChunkResult.java
@@ -1,9 +1,54 @@
 package io.rileyhe1.concurrency.Data;
-@SuppressWarnings("unused")
+
 public class ChunkResult
 {
-    private String tempFilePath;
-    private long bytesDownloaded;
-    private boolean success;
-    private Exception error;
+    private final String tempFilePath;
+    private final long bytesDownloaded;
+    private final boolean success;
+    private final Exception error;
+
+    private ChunkResult(String tempFilePath, long bytesDownloaded, boolean success, Exception error)
+    {
+        this.tempFilePath = tempFilePath;
+        this.bytesDownloaded = bytesDownloaded;
+        this.success = success;
+        this.error = error;
+    }
+    public String getTempFilePath()
+    {
+        return tempFilePath;
+    }
+    public long getBytesDownloaded()
+    {
+        return bytesDownloaded;
+    }
+    public boolean isSuccessFul()
+    {
+        return success;
+    }
+    public Exception getError()
+    {
+        return error;
+    }
+
+    public static ChunkResult success(String tempFilePath, long bytesDownloaded)
+    {
+        return new ChunkResult(tempFilePath, bytesDownloaded, true, null);
+    }
+    public static ChunkResult failure(Exception error)
+    {
+        return new ChunkResult(null, 0, false, error);
+    }
+    public static ChunkResult failure(Exception error, long bytesDownloaded)
+    {
+        return new ChunkResult(null, bytesDownloaded, false, error);
+    }
+    public boolean hasError() 
+    {
+        return this.error != null;
+    }
+    public String getErrorMessage()
+    {
+        return this.error != null? error.getMessage() : null; 
+    }
 }
