62ec274b0a5e4c2f57223c1182f8cb84dc5c907f
Riley Hendrickson <rileyhe1@gmail.com>
2025-11-26 19:50:00 -0800

    starting FileAssembler implementation

--- src/main/java/io/rileyhe1/concurrency/Util/FileAssembler.java
+++ src/main/java/io/rileyhe1/concurrency/Util/FileAssembler.java
@@ -1,5 +1,6 @@
 package io.rileyhe1.concurrency.Util;
 
+import java.io.IOException;
 import java.util.List;
 
 import io.rileyhe1.concurrency.Data.ChunkResult;
@@ -7,9 +8,24 @@ import io.rileyhe1.concurrency.Data.ChunkResult;
 @SuppressWarnings("unused")
 public class FileAssembler
 {
-    public void assembleChunks(List<ChunkResult> results, String destination)
+    public void assembleChunks(List<ChunkResult> results, String destination) throws IOException
     {
-        
+        validateChunks(results);
+
+    }
+
+    private void validateChunks(List<ChunkResult> results) throws IOException
+    {
+        if(results == null || results.isEmpty()) throw new IOException("Results list is null/contains no chunks");
+        for(int i = 0; i < results.size(); i++)
+        {
+            ChunkResult current = results.get(i);
+        }
+    }
+
+    private List<ChunkResult> sortChunks(List<ChunkResult> results)
+    {
+        return results;
     }
 
     private void mergeFiles(List<String> tempFiles, String output)
