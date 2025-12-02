fd6c6ec139208715e15fc5bcdea422cec9bf64bd
Riley Hendrickson <rileyhe1@gmail.com>
2025-12-02 14:32:01 -0800

    completed ProgressTracker implementation, needs testing validation before moving on

--- src/main/java/io/rileyhe1/concurrency/Util/ProgressTracker.java
+++ src/main/java/io/rileyhe1/concurrency/Util/ProgressTracker.java
@@ -1,25 +1,42 @@
 package io.rileyhe1.concurrency.Util;
 
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 @SuppressWarnings("unused")
 public class ProgressTracker
 {
-    private Map<String, AtomicLong> chunkProgress;
+    private final ConcurrentHashMap<Integer, AtomicLong> chunkProgress;
+    private long totalSize;
     
+    public ProgressTracker(long totalSize)
+    {
+        this.chunkProgress = new ConcurrentHashMap<>();
+        this.totalSize = totalSize;
+    }
+
     public void updateProgress(Integer chunkIndex, long bytes)
     {
-
+        if(chunkProgress.contains(chunkIndex))
+        {
+            chunkProgress.get(chunkIndex).addAndGet(bytes);
+        }
+        else
+        {
+            chunkProgress.put(chunkIndex, new AtomicLong(bytes));
+        }
     }
 
     public long getTotalProgress()
     {
-        return 0;
+        return chunkProgress.values().stream()
+                .mapToLong(AtomicLong::get)
+                .sum();
     }
 
     public double getProgressPercentage(long totalSize)
     {
-        return 0.0;
+        return (double) getTotalProgress() / totalSize * 100;
     }
 }
